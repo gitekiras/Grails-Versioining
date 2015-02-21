dedee31af32e37e3ad7972b258852b405d1a348c
Ekansh <ekansh.rastogi@intelligrape.com>
2015-02-21 18:05:58 +0530

    Versioning of Controlles using namespaces

---
+++ grails-app/controllers/com/ekiras/versioning/v1/TestController.groovy
@@ -0,0 +1,10 @@
+package com.ekiras.versioning.v1
+
+class TestController {
+
+    static namespace = "v1"
+
+    def index() {
+        render "Hi, This is version 1"
+    }
+}
---
+++ grails-app/controllers/com/ekiras/versioning/v2/TestController.groovy
@@ -0,0 +1,10 @@
+package com.ekiras.versioning.v2
+
+class TestController {
+
+    static namespace = "v2"
+
+    def index() {
+        render "Hi, This is version 2"
+    }
+}
